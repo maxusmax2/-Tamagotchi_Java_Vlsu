f072af9e40a1cce6c65548c968a2facb05cfe668
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-03-14 21:24:59 +0300

    Добавлены изображения

---
+++ src/Tamagochi/Game_window.java
@@ -0,0 +1,27 @@
+package Tamagochi;
+import java.awt.Image;
+import javax.swing.*;
+public class Game_window extends JFrame{
+        Image image = new ImageIcon("../../../Images/CH1_funny.jpg").getImage();
+        JLabel label_name_character =new JLabel();
+        JProgressBar progressBar_feed = new JProgressBar();
+        JProgressBar progressBar_play = new JProgressBar();
+        JProgressBar progressBar_sleep = new JProgressBar();
+        JProgressBar progressBar_clean = new JProgressBar();
+        JButton button_feed = new JButton();
+        JButton button_play = new JButton();
+        JButton button_sleep = new JButton();
+        JButton button_clean = new JButton();
+        JButton button_menu = new JButton();
+        JLabel label_days_of_life =new JLabel();
+
+        public void Game_window_show()
+        {
+                super();
+
+
+
+        }
+
+
+}
---
+++ src/Tamagochi/Main_class.java
@@ -0,0 +1,4 @@
+package Tamagochi;
+
+public class Main_class {
+}
