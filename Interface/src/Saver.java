92b35176ef039e79664156259c62e5403d3ae250
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-04-14 11:35:37 +0300

    Изменены методы downloadGame и saveGame

--- Interface/src/Saver.java
+++ Interface/src/Saver.java
@@ -1,41 +1,22 @@
 import java.io.*;
-import java.time.LocalDateTime;
 
 public class Saver {
     public Save infoLastSave = new Save();
 
     public void saveGame (Save parametersOfSave) throws IOException {
 
-        String gameStartDate = parametersOfSave.gameStartDate.toString();
-        String gameSaveDate = parametersOfSave.gameSaveDate.toString();
-        FileWriter gameInfoFile = new FileWriter("gameInfo.txt");
-        gameInfoFile.write(gameStartDate + "\n");
-        gameInfoFile.write(gameSaveDate+ "\n");
-        gameInfoFile.write(parametersOfSave.characterName+ "\n");
-        gameInfoFile.write(parametersOfSave.saveProgressValues.feedValue+"\n");
-        gameInfoFile.write(parametersOfSave.saveProgressValues.playValue+"\n");
-        gameInfoFile.write(parametersOfSave.saveProgressValues.sleepValue+"\n");
-        gameInfoFile.write(parametersOfSave.saveProgressValues.cleanValue+"\n");
-        gameInfoFile.close();
+        FileOutputStream fos = new FileOutputStream("gameInfo.txt");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(parametersOfSave);
+        oos.flush();
     }
     public void downloadGame () throws IOException {
-        FileReader gameInfoFile = new FileReader("gameInfo.txt");
-        BufferedReader readFile = new BufferedReader(gameInfoFile);
-        String s;
-        s=readFile.readLine();
-        infoLastSave.gameStartDate =  LocalDateTime.parse(s);
-        s=readFile.readLine();
-        infoLastSave.gameSaveDate =  LocalDateTime.parse(s);
-        s=readFile.readLine();
-        infoLastSave.characterName = s;
-        s=readFile.readLine();
-        infoLastSave.saveProgressValues.feedValue = Integer.parseInt(s);
-        s=readFile.readLine();
-        infoLastSave.saveProgressValues.playValue = Integer.parseInt(s);
-        s=readFile.readLine();
-        infoLastSave.saveProgressValues.sleepValue = Integer.parseInt(s);
-        s=readFile.readLine();
-        infoLastSave.saveProgressValues.cleanValue = Integer.parseInt(s);
-        gameInfoFile.close();
+        Object result = null;
+        try {
+            FileInputStream fis = new FileInputStream("gameInfo.txt");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            infoLastSave=(Save) ois.readObject(); }
+        catch(ClassNotFoundException e) {
+        }
     }
 }
